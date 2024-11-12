6a61733a4ee81ccccbba9f576d83013554233330
MertAygunn <mertaygun97@gmail.com>
2024-11-12 04:50:01 +0300

    Update sendEmail.js

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,38 +1,38 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer';
 
 export default function handler(req, res) {
-  if (req.method === "POST") {
-    const { name, email, phone, subject, message } = req.body;
+    if (req.method === 'POST') {
+        const { name, email, phone, subject, message } = req.body;
 
-    // Nodemailer ile e-posta gönderme ayarları
-    const transporter = nodemailer.createTransport({
-      host: "smtp.office365.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: process.env.OUTLOOK_USER, // Çevresel değişken
-        pass: process.env.OUTLOOK_PASSWORD, // Çevresel değişken
-      },
-    });
+        // Nodemailer ile e-posta gönderme ayarları
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.office365.com',
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: process.env.OUTLOOK_USER, // Çevresel değişken
+                pass: process.env.OUTLOOK_PASSWORD, // Çevresel değişken
+            },
+        });
 
-    // E-posta içeriği
-    const mailOptions = {
-      from: process.env.OUTLOOK_USER, // Gönderen e-posta adresi
-      to: email, // Alıcı e-posta adresi (formdan gelen)
-      subject: subject, // Formdan gelen konu
-      text: message, // Formdan gelen mesaj
-    };
+        // E-posta içeriği
+        const mailOptions = {
+            from: process.env.OUTLOOK_USER, // Gönderen e-posta adresi
+            to: email, // Alıcı e-posta adresi (formdan gelen)
+            subject: subject, // Formdan gelen konu
+            text: message, // Formdan gelen mesaj
+        };
 
-    // E-posta gönderme işlemi
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        return res.status(500).json({ error: error.message });
-      }
-      return res.status(200).json({ message: "E-posta başarıyla gönderildi!" });
-    });
-  } else {
-    // Eğer GET isteği gelirse 405 hatası döndür
-    res.setHeader("Allow", ["POST"]);
-    return res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+        // E-posta gönderme işlemi
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return res.status(500).json({ error: error.message });
+            }
+            return res.status(200).json({ message: 'E-posta başarıyla gönderildi!' });
+        });
+    } else {
+        // Eğer GET isteği gelirse 405 hatası döndür
+        res.setHeader('Allow', ['POST']);
+        return res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
 }
